03dc6495b85f00a7fe03f45e790386706742e395
rakesh000999 <rakeshjoshi6078@gmail.com>
2025-07-09 16:27:15 +0545

    internal and extenal CSS in react

--- my-react-project/src/components/LearnInlineCSS.jsx
+++ my-react-project/src/components/LearnInlineCSS.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../assets/css/style.css'
 
 const LearnInlineCSS = () => {
 
@@ -16,11 +17,37 @@ const LearnInlineCSS = () => {
 
     return (
         <>
+            <style>
+                {`
+                    .container{
+                        background-color: #00ff00;
+                        height: 100px;
+                        width: 100px;
+                    }
+                `}
+            </style>
+
+            <h1>Inline CSS</h1>
             <p style={{ fontSize: '50px', fontWeight: 600, color: 'green' }}>Hello, this is a paragraph.</p>
 
             <div style={style.container}></div>
 
             <h2 style={style.h2Text}>Text H2 Element</h2>
+
+            <hr />
+
+            <h1>Internal CSS</h1>
+            <div className='container'>
+
+            </div>
+
+            <hr />
+            <h1>External CSS</h1>
+
+            <div className='yellowbox'>
+
+            </div>
+            <h2 className='yellowtext'>Yellow Text</h2>
         </>
     )
 }
