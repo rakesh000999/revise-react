79acd9ef27b8a2fce205e6fc22a8eca2d99aa2c6
rakesh000999 <rakeshjoshi6078@gmail.com>
2025-07-09 19:21:40 +0545

    form best practise

--- my-react-project/src/components/LearnForms.jsx
+++ my-react-project/src/components/LearnForms.jsx
@@ -2,23 +2,48 @@ import React, { useState } from 'react'
 
 const LearnForms = () => {
 
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
+    // const [firstName, setFirstName] = useState('')
+    // const [lastName, setLastName] = useState('')
 
-    const handleFirstName = (e) => {
-        setFirstName(e.target.value)
+    // const handleFirstName = (e) => {
+    //     setFirstName(e.target.value)
+    // }
+
+    // const handleLastName = (e) => {
+    //     setLastName(e.target.value)
+    // }
+
+
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: ''
+    })
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
     }
 
-    const handleLastName = (e) => {
-        setLastName(e.target.value)
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form submitted with data:", formData);
     }
 
     return (
         <>
-            <form action="">
+            {/* <form action="">
                 First Name: <input type="text" name='firstName' onChange={handleFirstName} value={firstName} />
                 <br /><br />
                 Last Name: <input type="text" name='lastName' value={lastName} onChange={handleLastName} />
+            </form> */}
+
+
+            <form action="" onSubmit={handleFormSubmit}>
+                First Name: <input type="text" name='firstName' value={formData.firstName} onChange={handleChange} />
+                <br /><br />
+                Last Name: <input type="text" name='lastName' value={formData.lastName} onChange={handleChange} />
+                <br />
+                <br />
+                <input type="submit" value="Submit" />
             </form>
         </>
     )
